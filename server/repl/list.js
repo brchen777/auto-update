11a4ccd0dd45b0948788f0e14e265c883fbc6be6
brchen777 <eric790620@gmail.com>
2018-12-21 11:46:17 +0800

    Update repl list

--- server/repl/list.js
+++ server/repl/list.js
@@ -13,11 +13,32 @@
          * @example deviceList();
         */
         return () => {
-            let result = [];
+            let list = [];
             Object.entries(machineMap).forEach(([key, val]) => {
-                result.push([val.ip, val.uid]);
+                list.push([val.ip, val.uid]);
             });
-            return result;
+
+            // sort lastNum
+            list.sort((val1, val2) => {
+                let lastNumIdx1 = val1[0].lastIndexOf('.');
+                let lastNum1 = (lastNumIdx1 > 0) ? parseInt(val1[0].substring(lastNumIdx1 + 1)) : val1[0];
+
+                let lastNumIdx2 = val2[0].lastIndexOf('.');
+                let lastNum2 = (lastNumIdx2 > 0) ? parseInt(val2[0].substring(lastNumIdx2 + 1)) : val2[0];
+
+                if (lastNum1 < lastNum2) {
+                    return -1;
+                }
+                else if (lastNum1 > lastNum2) {
+                    return 1;
+                }
+                return 0;
+            });
+
+            return {
+                cnt: list.length,
+                list
+            };
         };
     };
 })();
